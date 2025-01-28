6c710c9e379c1136d50f0f8c5ec71420d2ed46ac
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-01-28 12:48:09 -0500

    debug

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def main():
                 if current_image == 7:
                     current_image = 8  # Branch 7 -> 8
                 elif current_image == 7.1:
-                    current_image = 7.2  # Branch 7.1 -> 8
+                    current_image = 7.2  # Branch 7.1 -> 7.2
                 elif current_image == 7.2:
                     current_image = 8  # Branch 7.2 -> 8
                 elif current_image == 9 or current_image == 7.3:
@@ -106,7 +106,7 @@ def main():
                 if current_image == 7:
                     current_image = 7.1  # Branch 7 -> 7.1
                 elif current_image == 7.1:
-                    current_image = 8  # Branch 7.1 -> 7.2
+                    current_image = 8  # Branch 7.1 -> 8
                 elif current_image == 7.2:
                     current_image = 7.3  # Branch 7.2 -> 7.3 (end of branch)
                 elif current_image > 1 and (current_image <= 7 or current_image > 7.3):
