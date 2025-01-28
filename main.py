37393d060b0d67c52ff692d9136876d92f6ae614
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-01-28 12:40:05 -0500

    updated logic

--- main.py
+++ main.py
@@ -86,11 +86,10 @@ def main():
 
         while True:
             if not up_button.value:  # Up button pressed
-                print("Up Button Pushed")
                 if current_image == 7:
                     current_image = 8  # Branch 7 -> 8
                 elif current_image == 7.1:
-                    current_image = 8  # Branch 7.1 -> 8
+                    current_image = 7.2  # Branch 7.1 -> 8
                 elif current_image == 7.2:
                     current_image = 8  # Branch 7.2 -> 8
                 elif current_image == 9 or current_image == 7.3:
@@ -104,11 +103,10 @@ def main():
                 display_image(images[current_image])
 
             if not down_button.value:  # Down button pressed
-                print("Down Button Pushed")
                 if current_image == 7:
                     current_image = 7.1  # Branch 7 -> 7.1
                 elif current_image == 7.1:
-                    current_image = 7.2  # Branch 7.1 -> 7.2
+                    current_image = 8  # Branch 7.1 -> 7.2
                 elif current_image == 7.2:
                     current_image = 7.3  # Branch 7.2 -> 7.3 (end of branch)
                 elif current_image > 1 and (current_image <= 7 or current_image > 7.3):
